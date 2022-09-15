13e5450bcd8f62428339230967c56ed56c99c044
hemhem <kimhp1216@gmail.com>
2022-09-15 10:09:20 +0900

    pf -ing

--- js/script.js
+++ js/script.js
@@ -1,57 +1,33 @@
 $(document).ready(function () {
 
-
-  // 탭 메뉴 swiper 기능
-  let swAbout2 = new Swiper('.sw-about-2', {
+  let swAbout2 = new Swiper('.sw-pf', {
     loop: true,
-    // display 변경시 처리
     observer: true,
     observeParents: true,
-    // pagination 적용
     pagination: {
-      el: '.sw-about-2-pg',
+      el: '.sw-pf-pg',
       clickable: true
     },
   });
 
   swAbout2.on('slideChange', function () {
-    // console.log('activeIndex', swAbout2.activeIndex);
-    // activeIndex 는 Swiper 가 복제한 슬라이드 번호만큼
-
-    // console.log('realIndex', swAbout2.realIndex);
-    // realIndex 는 html 코드 상의 슬라이드 갯수 만큼
-
-    // 일단 모두 포커스 class 지운다.
-    aboutMenu.removeClass('about-menu-focus');
-    // 나는 포커스 들어간다.
-    aboutMenu.eq(swAbout2.realIndex).addClass('about-menu-focus');
+    aboutMenu.removeClass('pf-listbox-focus');
+    aboutMenu.eq(swAbout2.realIndex).addClass('pf-listbox-focus');
 
   });
 
-  // 목록 버튼으로 슬라이드 제어하기  
-  // 목록
-  let aboutMenu = $('.about-menu-list > li > a');
-  aboutMenu.eq(0).addClass('about-menu-focus');
+  let aboutMenu = $('.pf-list > li > a');
+  aboutMenu.eq(0).addClass('pf-listbox-focus');
 
   $.each(aboutMenu, function (index, item) {
     $(this).click(function (event) {
-      // href 를 막는다.
       event.preventDefault();
-
-      // 일단 모두 포커스 class 지운다.
-      aboutMenu.removeClass('about-menu-focus');
-
-      // 나는 포커스 들어간다.
-      aboutMenu.eq(index).addClass('about-menu-focus');
-      // $(this).addClass('about-menu-focus');
-
-      // about2 슬라이더를 제어한다.
+      aboutMenu.removeClass('pf-listbox-focus');
+      aboutMenu.eq(index).addClass('pf-listbox-focus');
       swAbout2.slideTo(index + 1);
 
     });
   });
-
-
 
 
   let swiper = new Swiper(".mySwiper", {
@@ -69,13 +45,9 @@ $(document).ready(function () {
       clickable: true,
     },
   });
-
-
 });
 
 window.onload = function () {
-
-
   function makeCircle(_id, _str, _startColor, _endColor) {
     let bar = new ProgressBar.Circle(_id, {
       color: '#777',
@@ -131,16 +103,56 @@ window.onload = function () {
 
   AOS.init();
 
-  bar_html.animate(0.95);
-  bar_java.animate(0.7)
-  bar_css.animate(0.9);
-  bar_jquery.animate(0.8);
-  bar_scss.animate(0.8); 
-  bar_vue.animate(0.7);
-  bar_figma.animate(0.9);
-  bar_github.animate(0.9);
-  bar_photoshop.animate(0.9);
-  bar_illustrator.animate(0.95);
-  bar_afec.animate(0.8);
-  bar_premiere.animate(0.75);
+  let barAni = () => {
+    bar_html.animate(0.95);
+    bar_java.animate(0.7)
+    bar_css.animate(0.9);
+    bar_jquery.animate(0.8);
+    bar_scss.animate(0.8); 
+    bar_vue.animate(0.7);
+    bar_figma.animate(0.9);
+    bar_github.animate(0.9);
+    bar_photoshop.animate(0.9);
+    bar_illustrator.animate(0.95);
+    bar_afec.animate(0.8);
+    bar_premiere.animate(0.75);
+  }
+
+  let barAniRe = () => {
+    bar_html.animate(0);
+    bar_java.animate(0)
+    bar_css.animate(0);
+    bar_jquery.animate(0);
+    bar_scss.animate(0); 
+    bar_vue.animate(0);
+    bar_figma.animate(0);
+    bar_github.animate(0);
+    bar_photoshop.animate(0);
+    bar_illustrator.animate(0);
+    bar_afec.animate(0);
+    bar_premiere.animate(0);
+  }
+
+  new Waypoint({
+    element: $('.mySwiper .swiper-wrapper > div'),
+    handler: function (direction) {
+      if (direction == 'down') {
+        barAni();
+      } else if (direction == 'up') {
+        barAni();
+      }
+    },
+    offset: '80%'
+  });
+  new Waypoint({
+    element: $('.life'),
+    handler: function (direction) {
+      if (direction == 'down') {
+        barAniRe();
+      } else if (direction == 'up') {
+        barAni();
+      }
+    },
+    offset: '0%'
+  });
 }
