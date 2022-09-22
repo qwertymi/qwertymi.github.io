834b627a3b9411115d43f4373d8c22f998d12355
hemhem <kimhp1216@gmail.com>
2022-09-22 19:36:15 +0900

    vision&focus

--- js/script.js
+++ js/script.js
@@ -1,34 +1,30 @@
 $(document).ready(function () {
-
-  let swAbout2 = new Swiper('.sw-pf', {
+  let swAbout2 = new Swiper(".sw-pf", {
     loop: true,
     observer: true,
     observeParents: true,
     pagination: {
-      el: '.sw-pf-pg',
-      clickable: true
+      el: ".sw-pf-pg",
+      clickable: true,
     },
   });
 
-  swAbout2.on('slideChange', function () {
-    aboutMenu.removeClass('pf-listbox-focus');
-    aboutMenu.eq(swAbout2.realIndex).addClass('pf-listbox-focus');
-
+  swAbout2.on("slideChange", function () {
+    aboutMenu.removeClass("pf-listbox-focus");
+    aboutMenu.eq(swAbout2.realIndex).addClass("pf-listbox-focus");
   });
 
-  let aboutMenu = $('.pf-list > li > a');
-  aboutMenu.eq(0).addClass('pf-listbox-focus');
+  let aboutMenu = $(".pf-list > li > a");
+  aboutMenu.eq(0).addClass("pf-listbox-focus");
 
   $.each(aboutMenu, function (index, item) {
     $(this).click(function (event) {
       event.preventDefault();
-      aboutMenu.removeClass('pf-listbox-focus');
-      aboutMenu.eq(index).addClass('pf-listbox-focus');
+      aboutMenu.removeClass("pf-listbox-focus");
+      aboutMenu.eq(index).addClass("pf-listbox-focus");
       swAbout2.slideTo(index + 1);
-
     });
   });
-
 
   let swiper = new Swiper(".mySwiper", {
     slidesPerView: 5,
@@ -45,85 +41,84 @@ $(document).ready(function () {
       clickable: true,
     },
   });
-
 });
 
 window.onload = function () {
-
   new Waypoint({
-    element: $('.polygon-color'),
+    element: $(".polygon-color"),
     handler: function (direction) {
-      if (direction == 'down') {
-        $('.polygon-color').addClass('polygon-color-draw');
-      } else if (direction == 'up') {
-        $('.polygon-color').addClass('polygon-color-draw');
+      if (direction == "down") {
+        $(".polygon-color").addClass("polygon-color-draw");
+      } else if (direction == "up") {
+        $(".polygon-color").addClass("polygon-color-draw");
       }
     },
-    offset: '70%'
+    offset: "70%",
   });
-  
+
   AOS.init();
 
   function makeCircle(_id, _str, _startColor, _endColor) {
     let bar = new ProgressBar.Circle(_id, {
-      color: '#777',
+      color: "#777",
       strokeWidth: 50,
       trailWidth: 50,
-      easing: 'easeOut',
+      easing: "easeOut",
       duration: 1400,
 
       text: {
-        autoStyleContainer: false
+        autoStyleContainer: false,
       },
       from: {
         color: _startColor,
-        width: 50
+        width: 50,
       },
       to: {
         color: _endColor,
         width: 50,
       },
       step: function (state, circle) {
-        circle.path.setAttribute('stroke', state.color);
-        circle.path.setAttribute('stroke-width', state.width);
+        circle.path.setAttribute("stroke", state.color);
+        circle.path.setAttribute("stroke-width", state.width);
 
         let value = Math.round(circle.value() * 100);
         if (value === 0) {
-          circle.setText('');
+          circle.setText("");
         } else {
-          circle.setText(_str + '<br>' + value);
+          circle.setText(_str + "<br>" + value);
         }
-
-      }
+      },
     });
-    bar.text.style.fontFamily = 'LeferiBase';
-    bar.text.style.fontSize = '24px';
-    bar.text.style.color = '#333';
+    bar.text.style.fontFamily = "LeferiBase";
+    bar.text.style.fontSize = "24px";
+    bar.text.style.color = "#333";
     return bar;
-
   }
 
-  let bar_html = makeCircle(html, 'HTML', '#e73c7e', '#C9D1FF');
-  let bar_js = makeCircle(js, 'JS', '#e73c7e', '#C9D1FF');
-  let bar_css = makeCircle(css, 'CSS', '#e73c7e', '#C9D1FF');
-  let bar_jquery = makeCircle(jquery, 'JQuery','#e73c7e', '#C9D1FF');
-  let bar_scss = makeCircle(scss, 'SCSS', '#e73c7e', '#C9D1FF');
-  let bar_vue = makeCircle(vue, 'Vue.js', '#e73c7e', '#C9D1FF');
-  let bar_figma = makeCircle(figma, 'Figma', '#e73c7e', '#C9D1FF');
-  let bar_github = makeCircle(github, 'Github', '#e73c7e', '#C9D1FF');
-  let bar_photoshop = makeCircle(photoshop, 'Phtoshop', '#e73c7e', '#C9D1FF');
-  let bar_illustrator = makeCircle(illustrator, 'Illustrator', '#e73c7e', '#C9D1FF');
-  let bar_afec = makeCircle(afec, 'After Effect', '#e73c7e', '#C9D1FF');
-  let bar_premiere = makeCircle(premiere, 'Premiere', '#e73c7e', '#C9D1FF');
-
-
+  let bar_html = makeCircle(html, "HTML", "#e73c7e", "#C9D1FF");
+  let bar_js = makeCircle(js, "JS", "#e73c7e", "#C9D1FF");
+  let bar_css = makeCircle(css, "CSS", "#e73c7e", "#C9D1FF");
+  let bar_jquery = makeCircle(jquery, "JQuery", "#e73c7e", "#C9D1FF");
+  let bar_scss = makeCircle(scss, "SCSS", "#e73c7e", "#C9D1FF");
+  let bar_vue = makeCircle(vue, "Vue.js", "#e73c7e", "#C9D1FF");
+  let bar_figma = makeCircle(figma, "Figma", "#e73c7e", "#C9D1FF");
+  let bar_github = makeCircle(github, "Github", "#e73c7e", "#C9D1FF");
+  let bar_photoshop = makeCircle(photoshop, "Phtoshop", "#e73c7e", "#C9D1FF");
+  let bar_illustrator = makeCircle(
+    illustrator,
+    "Illustrator",
+    "#e73c7e",
+    "#C9D1FF"
+  );
+  let bar_afec = makeCircle(afec, "After Effect", "#e73c7e", "#C9D1FF");
+  let bar_premiere = makeCircle(premiere, "Premiere", "#e73c7e", "#C9D1FF");
 
   let barAni = () => {
     bar_html.animate(0.95);
-    bar_js.animate(0.72)
+    bar_js.animate(0.72);
     bar_css.animate(0.91);
     bar_jquery.animate(0.78);
-    bar_scss.animate(0.8); 
+    bar_scss.animate(0.8);
     bar_vue.animate(0.7);
     bar_figma.animate(0.9);
     bar_github.animate(0.9);
@@ -131,14 +126,14 @@ window.onload = function () {
     bar_illustrator.animate(0.93);
     bar_afec.animate(0.8);
     bar_premiere.animate(0.75);
-  }
+  };
 
   let barAniRe = () => {
     bar_html.animate(0);
-    bar_js.animate(0)
+    bar_js.animate(0);
     bar_css.animate(0);
     bar_jquery.animate(0);
-    bar_scss.animate(0); 
+    bar_scss.animate(0);
     bar_vue.animate(0);
     bar_figma.animate(0);
     bar_github.animate(0);
@@ -146,29 +141,71 @@ window.onload = function () {
     bar_illustrator.animate(0);
     bar_afec.animate(0);
     bar_premiere.animate(0);
-  }
+  };
 
   new Waypoint({
-    element: $('.mySwiper .swiper-wrapper > div'),
+    element: $(".mySwiper .swiper-wrapper > div"),
     handler: function (direction) {
-      if (direction == 'down') {
+      if (direction == "down") {
         barAni();
-      } else if (direction == 'up') {
+      } else if (direction == "up") {
         barAniRe();
       }
     },
-    offset: '90%'
+    offset: "90%",
   });
-  
+
+  let focusRemove = () => {
+    $(".gnb li a").removeClass("menu-focus");
+  };
   new Waypoint({
-    element: $('.portfolio'),
+    element: $(".profile"),
     handler: function (direction) {
-      if (direction == 'down') {
-        barAniRe();
-      } else if (direction == 'up') {
-        barAni();
+      focusRemove();
+      if (direction == "down") {
+        $(".profile-menu").addClass("menu-focus");
+      } else if (direction == "up") {
+        $(".profile-menu").addClass("menu-focus");
       }
     },
-    offset: '10%'
+    offset: "0%",
   });
-}
+  new Waypoint({
+    element: $(".portfolio"),
+    handler: function (direction) {
+      focusRemove();
+      if (direction == "down") {
+        $(".portfolio-menu").addClass("menu-focus");
+      } else if (direction == "up") {
+        $(".profile-menu").addClass("menu-focus");
+      }
+    },
+    offset: "60%",
+  });
+  new Waypoint({
+    element: $(".life"),
+    handler: function (direction) {
+      focusRemove();
+      if (direction == "down") {
+        $(".life-menu").addClass("menu-focus");
+      } else if (direction == "up") {
+        $(".portfolio-menu").addClass("menu-focus");
+      }
+    },
+    offset: "60%",
+  });
+
+  new Waypoint({
+    element: $(".vision"),
+    handler: function (direction) {
+      if (direction == "down") {
+        $(".vision-box").addClass("bigEntrance");
+        $(".line").addClass('line-draw');
+      } else if (direction == "up") {
+        $(".vision-box").addClass("bigEntrance");
+        $(".line").addClass("line-draw");
+      }
+    },
+    offset: "40%",
+  });
+};
